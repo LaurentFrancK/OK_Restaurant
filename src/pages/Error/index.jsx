8562b3049b5c1cdd4eed7cbb62eddd91896866af
LaurentFrancK <okouyikongol@gmail.com>
2025-04-14 22:48:07 +0100

    Ajout de l'image illustrant l'erreur 404 sur notre page d'erreurs

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,9 +1,31 @@
+// Import react's components
+import {styled} from 'styled-components'
+
+// Import project's components
+import errorLogo from '../../assets/images/404.png'
+
+// CSS rules
+
+const ErrorBloc = styled.div`
+margin: 26px auto;
+width: 90%;
+height: 80vh;
+display: flex;
+align-items: center;
+justify-content: center`
+
+const ImageComponent = styled.img`
+width: 40%;
+height: 100%;
+border-radius: 20px`
 
 
 
 function Error() {
     return (
-        <h1>Erreur 404</h1>
+        <ErrorBloc>
+            <ImageComponent src={errorLogo} alt="error page" />
+        </ErrorBloc>
     )
 }
 
